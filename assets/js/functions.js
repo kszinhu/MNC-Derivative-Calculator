88c8319ef65d9556bcc5160b8fd655b08f5972d8
Kszinhu <rodrigues.cassiano2007@gmail.com>
2021-06-17 16:55:45 -0300

    ✏️ - ah man, fix ae

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -79,9 +79,10 @@ function calculateSecondDerivative(input, x, epsilon) {
 }
 
 function calculatePartialDerivative(input, variables, i, epsilon) {
+  let x = getValueX();
   let h = 1000 * epsilon;
-  let xi = variables[i];
-  variables[i] = xi + h;
+  let xi = x[i];
+  x[i] = xi + h;
 
   f1 = evaluate(formattingExpression(input), variables);
   n[i] = xi - h;
@@ -107,34 +108,43 @@ evaluate = (input, variables) => {
   with (variables) with (Math) return eval(input);
 };
 
-getObjectVariables = (input) =>
+getSize = (input) => input.match(/\b([a-df-z])\d*\b/gi).length;
+
+getValueX = () =>
+  Array.from(document.querySelectorAll(".xValues")).map((element) => element.innerHTML);
+
+
+getObjectVariables = (input, x_values) =>
   input
     .match(/\b([a-df-z])\d*\b/gi)
-    .reduce((acc, cur) => ((acc[cur] = cur), acc), {});
+    .reduce(
+      (acc, cur, index) => ((acc[cur] = parseFloat(x_values[index])), acc),
+      {}
+    );
 
-blurCollapse = (input, counter) => {
-  debugger;
+blurCollapse = (input) => {
   if (!isNaN(parseFloat(input.value))) {
     let newElement = document.createElement("span");
     newElement.innerHTML = input.value;
-    newElement.setAttribute(`data-x${counter}`, input.value);
+    newElement.setAttribute(`data-x`, input.value);
+    newElement.setAttribute("class", "xValues");
     input.parentNode.replaceChild(newElement, input);
   }
 };
 
-createInput = (object, element_class, counter = 1) => {
+createInput = (size, element_class, counter = 1) => {
   let element = document.querySelector(element_class);
-  for (let key in object) {
+  for (let index = 0; index < size; index++) {
     let newElement = document.createElement("th");
-    newElement.innerHTML = `<input class="table-input" type="number" onblur="blurCollapse(this, ${counter})"></input>`;
+    newElement.innerHTML = `<input class="table-input" type="number" onblur="blurCollapse(this)"></input>`;
     element.appendChild(newElement);
     counter++;
   }
 };
 
-createLabels = (object, element_class, counter = 1) => {
+createLabels = (size, element_class, counter = 1) => {
   let element = document.querySelector(element_class);
-  for (let key in object) {
+  for (let index = 0; index < size; index++) {
     let newElement = document.createElement("th");
     newElement.innerHTML = `${counter}`;
     element.appendChild(newElement);
--- assets/js/main.js
+++ assets/js/main.js
@@ -18,16 +18,18 @@ function plotGradient() {
   // const tableResult = document.querySelector('.result-x');
 
   // tests only
-  let variables = getObjectVariables(document.querySelector("#Function").value); // return object
-  createLabels(variables);
+  let variables = getObjectVariables(
+    formattingExpression(document.querySelector("#Function").value),
+    getValueX()
+  ); // return object
 }
 
 function insertTable() {
   let form = document.querySelector("#form");
   let card = document.createElement("div");
-  let variables = getObjectVariables(
+  let size = getSize(
     formattingExpression(document.querySelector("#Function").value)
-  ); // Objeto com as váriáveis
+  ); // Tamanho das variáveis
   card.setAttribute("class", "card default insert-table");
   card.innerHTML = `<span class="title">Insira os valores das variáveis</span>
     <div class="table input-x">
@@ -46,6 +48,6 @@ function insertTable() {
   !document.querySelector(".insert-table")
     ? form.insertBefore(card, document.querySelector("#submit")) // True - Se não existir
     : form.replaceChild(card, document.querySelector(".insert-table")); // False - Se existir
-  createLabels(variables, ".row-label");
-  createInput(variables, ".input-label");
+  createLabels(size, ".row-label");
+  createInput(size, ".input-label");
 }

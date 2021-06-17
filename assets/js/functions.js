48b394c4bdb417807cd64aa47995dc0489179fa4
Kszinhu <rodrigues.cassiano2007@gmail.com>
2021-06-16 23:03:25 -0300

    ✅ - eval upgrade

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -76,3 +76,30 @@ function calculateSecondDerivative(input, x, epsilon) {
   }
   return p;
 }
+
+function calculatePartialDerivative(input, n, x, i, epsilon) {
+  let h = 1000 * epsilon;
+  let xi = n[i];
+  n[i] = xi + h;
+
+  f1 = evaluate(formattingExpression(input), variables);
+  n[i] = xi - h;
+  with (Math) f2 = eval(formattingExpression(input, x));
+  p = (f1 - f2) / (2 * h);
+
+  for (let index = 0; index < 10; index++) {
+    q = p;
+    h = h / 2;
+    n[i] = xi + h;
+    with (Math) f1 = eval(formattingExpression(input, x));
+    n[i] = xi - h;
+    with (Math) f2 = eval(formattingExpression(input, x));
+    p = (f1 - f2) / (2 * h);
+    if (Math.abs(p - q) < epsilon) break;
+  }
+  return p;
+}
+
+var evaluate = (input, variables) => {
+  with (variables) with (Math) return eval(input);
+};
